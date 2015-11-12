d4c684a6f4dffb8868669fa041fbdd307c6cd481
Albert Pak <al@albert-pak.com>
2015-11-11 18:00:43 -0600

    use  operator to change  attribute without changing whole document

--- leaderboard.js
+++ leaderboard.js
@@ -18,12 +18,13 @@ if (Meteor.isClient) {
   Template.leaderboard.events({
     'click li.player': function() {
       var playerId = this._id;
-
       Session.set('selectedPlayer', playerId);
-
       var selectedPlayer = Session.get('selectedPlayer');
+    },
 
-      console.log(selectedPlayer);
+    'click #increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({_id: selectedPlayer}, {$inc: {score: 5}});
     },
   });
 }
